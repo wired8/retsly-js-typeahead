705f35e2298881b2210c9e6b0eefdef4e44758cb
Kyle Campbell <mail@slajax.com>
2013-11-26 17:04:48 -0800

    commented out

---
+++ index.js
@@ -0,0 +1,30 @@
+
+//var autocomplete = require('autocomplete');
+
+module.exports = exports = function(retsly) {
+/*
+ *
+ *  return function(el) {
+ *
+ *    var url = [
+ *      'https://',
+ *      retsly.host,
+ *      '/api/v1/vendor',
+ *      '?client_id='+retsly.client_id,
+ *      '&access_token='+retsly.getToken()
+ *    ].join('');
+ *
+ *    autocomplete(el, url)
+ *      .parse(function(res) {
+ *        console.log(res);
+ *        return res;
+ *
+ *      })
+ *      .key('name')
+ *      .on('select', function(res) {
+ *        console.log(res);
+ *      });
+ *  }
+ */
+
+}
